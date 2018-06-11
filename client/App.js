608899617f370dab06898a7e3880e093f8ee2d43
Stanford <stanford@stanfordlin.com>
2018-06-11 16:43:49 -0700

    broken

--- client/App.js
+++ client/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import AceEditor from 'react-ace';
 import 'brace/theme/monokai';
 
+var sharedb = require('sharedb/lib/client');
+var StringBinding = require('sharedb-string-binding');
+
+// Open WebSocket connection to ShareDB server
+var socket = new WebSocket('ws://' + window.location.host);
+var connection = new sharedb.Connection(socket);
+
+var doc = connection.get('examples', 'textarea');
+doc.subscribe(function(err) {
+    if (err) throw err;
+    var element = document.querySelector('textarea');
+    var binding = new StringBinding(element, doc);
+    binding.setup();
+});
+
+
 import CustomOpenEdgeMode from './CustomOpenEdgeMode';
 // App component - represents the whole app
 class App extends Component {
