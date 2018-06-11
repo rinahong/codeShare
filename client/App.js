7bd3e50ec2a014ddc076568b7d67dfff9477b593
Stanford <stanford@stanfordlin.com>
2018-06-11 01:03:08 -0700

    Working with a custom rules regex error 2

--- client/App.js
+++ client/App.js
@@ -1,45 +1,37 @@
 import React, { Component } from 'react';
 import AceEditor from 'react-ace';
-import brace from 'brace';
+import 'brace/theme/monokai';
 
-
-
-import Task from './Task.js';
-
+import CustomOpenEdgeMode from './CustomOpenEdgeMode';
 // App component - represents the whole app
-export default class App extends Component {
-    getTasks() {
-        return [
-            { _id: 1, text: 'This is task 1' },
-            { _id: 2, text: 'This is task 2' },
-            { _id: 3, text: 'This is task 3' },
-        ];
-    }
+class App extends Component {
 
-    renderTasks() {
-        return this.getTasks().map((task) => (
-            <Task key={task._id} task={task} />
-        ));
+    componentDidMount() {
+        const customMode = new CustomOpenEdgeMode();
+        this.refs.aceEditor.editor.getSession().setMode(customMode);
     }
 
     render() {
         return (
             <div className="container">
-                <header>
-                    <h1>Todo List</h1>
-                </header>
 
-                <ul>
-                    {this.renderTasks()}
-                </ul>
 
                 <AceEditor
+                    ref="aceEditor"
+                    theme="monokai"
                     mode="java"
-                    theme="github"
-                    name="blah1"
-                    height="6em"
+                    highlightActiveLine={true}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                        showLineNumbers: true,
+                        tabSize: 3,
+                    }}
+
                 />
             </div>
         );
     }
 }
+
+export default App;
