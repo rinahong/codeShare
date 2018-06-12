6711deb1ecbba85cdbc60da1be94391492c1fd0a
Stanford <stanford@stanfordlin.com>
2018-06-12 09:43:14 -0700

    broken

--- client/App.js
+++ client/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import AceEditor from 'react-ace';
 import 'brace/theme/monokai';
+import 'brace/mode/javascript';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Tasks } from '../imports/api/tasks.js';
 
 import CustomOpenEdgeMode from './CustomOpenEdgeMode';
 // App component - represents the whole app
 class App extends Component {
 
-    componentDidMount() {
-        const customMode = new CustomOpenEdgeMode();
-        this.refs.aceEditor.editor.getSession().setMode(customMode);
+    renderTasks() {
+        return this.props.tasks.map((task) => (
+          <Task key={task._id} task={task} />
+        ));
     }
+    
+
+    // componentDidMount() {
+    //     const customMode = new CustomOpenEdgeMode();
+    //     this.refs.aceEditor.editor.getSession().setMode(customMode);
+    // }
 
     render() {
         return (
+
+
+
             <div className="container">
 
+            <header>
+                <h1>Todo List</h1>
+            </header>
+
+            <ul>
+                {this.renderTasks()}
+            </ul>
 
                 <AceEditor
                     ref="aceEditor"
                     theme="monokai"
-                    mode="java"
+                    mode="javascript"
                     highlightActiveLine={true}
                     setOptions={{
                         enableBasicAutocompletion: true,
@@ -34,4 +54,8 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withTracker(() => {
+    return {
+      tasks: Tasks.find({}).fetch(),
+    };
+  })(App);
