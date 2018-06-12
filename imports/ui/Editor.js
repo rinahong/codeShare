fbd0902b535c04a16a04181562f6cb2302241b47
Faizal Karim <faizalk@imac.lan>
2018-06-11 23:39:40 -0700

    updated Editor.js to save data to documents collection; removed deprecated files

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -3,17 +3,30 @@ import { render } from 'react-dom';
 import brace from 'brace';
 import AceEditor from 'react-ace';
 import { Redirect } from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Tracker } from 'meteor/tracker'
+import _ from 'lodash';
+
+import { Documents } from '../api/documents.js';
 
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
 // Render editor
-export default class Editor extends Component {
+class Editor extends Component {
+
   constructor(props) {
     super(props);
 
     this.getHeight = this.getHeight.bind(this);
     this.getWidth = this.getWidth.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onLoad = this.onLoad.bind(this);
+
+    this.state = {
+      id: this.props.match.params.id
+    }
+
   }
 
   getHeight() {
@@ -24,11 +37,36 @@ export default class Editor extends Component {
     return window.innerWidth + "px";
   }
 
+  onChange(value, event) {
+    Documents.insert({
+      id: this.state.id,
+      value,
+      createdAt: new Date(), // current time
+    });
+  }
+
+  onLoad(editor) {
+    let document = Documents.findOne({id: this.state.id}, {sort: {createdAt: -1, limit: 1}});
+    if (document) {
+      editor.setValue(document.value);
+    }
+    /*
+    Tracker.autorun(() => {
+       let document = Documents.findOne({id: this.state.id}, {sort: {createdAt: -1, limit: 1}});
+       if (document) {
+         editor.setValue(document.value);
+       }
+    });
+    */
+  }
 
   render() {
+    console.log(this.props.document);
+
     const {id} = this.props.match.params; //document ID
     const height = this.getHeight(); //window height
     const width = this.getWidth(); //window width
+    const value = this.props.document ? this.props.document.value : '';
 
     // check is user is logged in; if not, redirect to login page
     if (Meteor.userId() == null) {
@@ -42,17 +80,22 @@ export default class Editor extends Component {
         )
     }
 
+    const onChange = _.debounce((value, event) => {this.onChange(value, event)}, 3000);
+
     return (
       <AceEditor
       mode="javascript"
       theme="monokai"
       name="editor"
+      //onLoad={this.onLoad}
+      onChange={onChange}
       fontSize={14}
       showPrintMargin={false}
       showGutter={true}
       highlightActiveLine={true}
       height={height}
       width={width}
+      value={value}
       setOptions={{
         enableBasicAutocompletion: true,
         enableLiveAutocompletion: true,
@@ -63,3 +106,12 @@ export default class Editor extends Component {
     );
   }
 }
+
+export default withTracker(() => {
+  if (this.state) {
+    return {
+      document: Documents.findOne({id: this.state.id}, {sort: {createdAt: -1, limit: 1}})
+    };
+  }
+  return {};
+})(Editor);
--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 
+import '../imports/api/documents.js';
+
 Meteor.startup(() => {
 });
