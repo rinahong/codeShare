3191c1161beb656481bf2157ffbff9bf57885de7
Rina Hong <rhong1006@gmail.com>
2018-06-14 22:47:26 -0700

    Create mongodb collection for DocumentContent Table.

--- server/main.js
+++ server/main.js
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/documents.js';
 import '../imports/api/userDoc.js';
 import '../imports/api/messages.js';
+import '../imports/api/DocumentContent.js';
 
 Meteor.startup(() => {
 });
