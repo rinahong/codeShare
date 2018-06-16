8fcbeddc190a5948d6cf072d5156e792a0f7f1d3
Stanford <stanford@stanfordlin.com>
2018-06-15 22:09:40 -0700

    Added theme colours

--- client/main.js
+++ client/main.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Editor from '../imports/ui/Editor.js';
 import { Landing } from '../imports/ui/Landing';
+import { createMuiTheme } from '@material-ui/core/styles';
+
 
 // import AccountsUIWrapper from '../imports/ui/AccountsUIWrapper.js';
 
@@ -12,6 +14,22 @@ import {SignIn} from '../imports/ui/SignIn';
 import NavBar2 from '../imports/ui/NavBar2';
 // import InsetList from '../imports/ui/InsetList';
 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#ff4400',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contast with palette.primary.main
+    },
+    secondary: {
+      main: '#a8b324',
+      // dark: will be calculated from palette.secondary.main,
+    },
+    // error: will use the default color
+  },
+});
 
 Meteor.startup(() => {
 
