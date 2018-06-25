40ee72b8bd51bc49d84146c1541eab4950c501e5
Rina Hong <rhong1006@gmail.com>
2018-06-24 19:59:48 -0700

    Refactor userSelection.js to clean up unused lines

--- imports/ui/UserSelection.js
+++ imports/ui/UserSelection.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 
@@ -10,21 +9,11 @@ export class UserSelection extends Component {
 
     this.state = {
 			allUsers: [],
-			removeSelected: true,
-			disabled: false,
-			stayOpen: false,
 			value: [],
-			rtl: false,
     }
 
 		this.handleSelectChange = this.handleSelectChange.bind(this);
 		this.toggleCheckbox = this.toggleCheckbox.bind(this);
-		this.toggleRtl = this.toggleRtl.bind(this);
-
-		// propTypes: {
-		// 	hint: PropTypes.string,
-		// 	label: PropTypes.string
-		// },
 
   }
 
@@ -50,47 +39,22 @@ export class UserSelection extends Component {
 			[e.target.name]: e.target.checked,
 		});
 	}
-	toggleRtl (e) {
-		let rtl = e.target.checked;
-		this.setState({ rtl });
-	}
 
 	render () {
-		const { disabled, stayOpen, value, allUsers } = this.state;
+		const { value, allUsers } = this.state;
 		const options = allUsers;
 		return (
 			<div className="section">
 				<Select
-					closeOnSelect={!stayOpen}
-					disabled={disabled}
+					closeOnSelect={false}
 					multi
 					onChange={this.handleSelectChange}
 					options={options}
-					placeholder="Select your favourite(s)"
-          removeSelected={this.state.removeSelected}
-					rtl={this.state.rtl}
+					placeholder="Select user(s)"
+          removeSelected={true}
 					simpleValue
 					value={value}
 				/>
-
-				<div className="checkbox-list">
-					<label className="checkbox">
-						<input type="checkbox" className="checkbox-control" name="removeSelected" checked={this.state.removeSelected} onChange={this.toggleCheckbox} />
-						<span className="checkbox-label">Remove selected options</span>
-					</label>
-					<label className="checkbox">
-						<input type="checkbox" className="checkbox-control" name="disabled" checked={this.state.disabled} onChange={this.toggleCheckbox} />
-						<span className="checkbox-label">Disable the control</span>
-					</label>
-					<label className="checkbox">
-						<input type="checkbox" className="checkbox-control" name="stayOpen" checked={stayOpen} onChange={this.toggleCheckbox}/>
-						<span className="checkbox-label">Stay open when an Option is selected</span>
-					</label>
-					<label className="checkbox">
-						<input type="checkbox" className="checkbox-control" name="rtl" checked={this.state.rtl} onChange={this.toggleCheckbox} />
-						<span className="checkbox-label">rtl</span>
-					</label>
-				</div>
 			</div>
 		);
 	}
