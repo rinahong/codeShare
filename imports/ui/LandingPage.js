d8f6d689436746f3866a900d0634b006c4da5c08
Stanford <stanford@stanfordlin.com>
2018-07-01 02:04:04 -0700

    added shared documents ui

--- imports/ui/LandingPage.js
+++ imports/ui/LandingPage.js
@@ -151,24 +151,9 @@ export class LandingPage extends Component {
             <AddIcon />
           </Button>
         </div>
-
         <List style={{ width: '85%', backgroundColor: 'paper', textAlign: 'center' }} subheader={<ListSubheader style={{ textAlign: 'left' }}component="div">Documents</ListSubheader>}>
           {
             this.state.documents.map(doc => (
-              // <ListItem button key={doc._id}>
-              //   <ListItemIcon>
-              //   </ListItemIcon>
-              //   <Link to={`/documents/${doc._id}`}>
-              //     <ListItemText inset primary={JSON.stringify(doc.title) + "---" + JSON.stringify(doc._id)} />
-
-              //   </Link>
-              //   <ListItemSecondaryAction onClick={this.deleteDocument(doc._id)}>
-              //     <IconButton aria-label="Comments">
-              //       <CommentIcon />
-              //     </IconButton>
-              //   </ListItemSecondaryAction>
-              // </ListItem>
-
               <ListItem key={doc._id}>
                 <ListItemAvatar>
                   <Avatar>
@@ -193,18 +178,30 @@ export class LandingPage extends Component {
 
         <hr/>
 
-        <h2>Shared Documents</h2>
-        <ul style={{paddingLeft: '10px'}}>
+
+          <List style={{ width: '85%', backgroundColor: 'paper', textAlign: 'center' }} subheader={<ListSubheader style={{ textAlign: 'left' }}component="div">Shared Documents</ListSubheader>}>
           {
             this.state.sharedDocuments.map(sharedDoc => (
-              <li key={sharedDoc._id}>
-                <Link to={`/documents/${sharedDoc._id}`}>
-                  {sharedDoc.title}
-                </Link>
-              </li>
+              <ListItem key={sharedDoc._id}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <InsertDriveFileIcon />
+                  </Avatar>
+                </ListItemAvatar>
+
+                  <Link to={`/documents/${sharedDoc._id}`}>
+                      <ListItemText inset primary={JSON.stringify(sharedDoc.title) + "---" + JSON.stringify(sharedDoc._id)}  secondary={"Created: "+(sharedDoc.createdAt.toLocaleDateString('en-US', DATE_OPTIONS))} />
+                  </Link>
+                <ListItemSecondaryAction onClick={this.deleteDocument(sharedDoc._id)}>
+
+                  <IconButton aria-label="Delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
             ))
           }
-        </ul>
+        </List>
 
       </div>
 
